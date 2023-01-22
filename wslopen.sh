328964e66fb9d9c02e41697a79fb05cce6e27fd8
rbamb <1152575283@qq.com>
2023-01-22 20:45:35 +0800

    add file:// protocol support and ./.. directory support

--- wslopen.sh
+++ wslopen.sh
@@ -1,83 +1,69 @@
 #! /usr/bin/env bash
 
-# https://github.com/cpbotha/xdg-open-ws
-
-VERSION="0.0.1"
+VERSION="0.0.2"
+PROG="wslopen"
 WIN_CMD="/mnt/c/Windows/System32/cmd.exe"
-DISTRO="$WSL_DISTRO_NAME"
-PROG=wslopen
-URL_REGEXP='(https?|ftp|file)://[-[:alnum:]\+&@#/%?=~_|!:,.;]*[-[:alnum:]\+&@#/%=~_|]'
-VERBOSE_MODE=0
-VALID_ARGS=$(getopt -o vbh --long version,verbose,'help' -- "$@")
+DISTRO=$WSL_DISTRO_NAME
 
-function wslopen_help() {
-    echo "wslopen: open files, directories and urls inside wsl/wsl2 using host apps"
-    echo "    usage: $PROG [-v | --version] [-b | --verbose] [-h | --help] [file | directory | url]"
+wslopen_version() {
+  echo "$PROG version $VERSION"
 }
 
-function wslopen_version() {
-    echo "$PROG version $VERSION"
+wslopen_help() {
+  echo "wslopen: open files, directories and urls inside wsl/wsl2 using host apps"
+  echo "    usage: $PROG [-v | -V | --version] [-h | --help] [file] [directory] [url]"
 }
 
-function wslopen_get_file_uri() {
-    local filepath=$(readlink -f "$1")
-    local win_filepath=${filepath//[\/]/\\\\\\\\}
-    echo "\\\\\\\\wsl$\\\\\\\\$DISTRO"$win_filepath""
+wslfile2win() {
+  local wslpath="$1"
+  local abswslpath=$(readlink -f "$wslpath")
+  local winpath=${abswslpath//[\/]/\\\\\\\\}
+  winpath="\\\\\\\\wsl$\\\\\\\\$DISTRO"$winpath""
+  echo "$winpath"
 }
 
-function wslopen_get_dir_uri() {
-    local filepath=$(readlink -f "$1")
-    local win_filepath=${filepath//[\/]/\\\\}
-    echo "\\\\\\\\wsl$\\\\$DISTRO"$win_filepath""
+wsldir2win() {
+  local wslpath="$1"
+  local abswslpath=$(readlink -f "$wslpath")
+  local winpath=${abswslpath//[\/]/\\\\}
+  winpath="\\\\\\\\wsl$\\\\$DISTRO"$winpath""
+  echo "$winpath"
 }
 
-function wslopen_get_url_uri() {
-    echo "$1"
+wslopen_https_proto() {
+  echo "$1"
 }
 
-if [[ $? -ne 0 ]]; then
-    echo "[ERROR] getopt returns error: $?"
-    exit 1;
-elif [[ $# -eq 0 ]]; then
-    wslopen_help
-    exit 1;
+wslopen_file_proto() {
+  local fileurl="$1"
+  local filepath="$(echo "$fileurl" | cut -c8-)"
+  echo $(wslfile2win "$filepath")
+}
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+  wslopen_help
+  exit 0
+elif [ "$1" == "-v" ] || [ "$1" == "--version" ]; then
+  wslopen_version
+  exit 0
+elif [ "$#" -eq 0 ]; then
+  wslopen_help
+  exit 0
 fi
 
-eval set -- "$VALID_ARGS"
-while [ : ]; do
-    case "$1" in
-        -v|--version)
-            wslopen_version
-            exit 1
-            ;;
-        -h|--help)
-            wslopen_help
-            exit 1
-            ;;
-        -b|--verbose)
-            VERBOSE_MODE=1
-            shift
-            ;;
-        *)
-            WSLOPEN_INPUT="$2"
-            shift;
-            break
-            ;;
-    esac
+for something in "$@"; do
+  if [[ "$something" == http://* ]] || [[ "$something" == https://* ]]; then
+    target=$(wslopen_https_proto "$something")
+  elif [[ "$something" == file://* ]]; then
+    target=$(wslopen_file_proto "$something")
+  elif [[ -f "$something" ]]; then
+    target=$(wslfile2win "$something")
+  elif [[ -d "$something" ]] || [[ "$something" == \.+ ]]; then
+    target=$(wsldir2win "$something")
+  else
+    echo "not a valid input: $something"
+    continue
+  fi
+  eval "$WIN_CMD /c start \"\" \"$target\" >/dev/null 2>/dev/null"
 done
-
-if [[ "$WSLOPEN_INPUT" =~ "$URL_REGEXP" ]]; then
-    [ $VERBOSE_MODE == 1 ] && echo "opened url "$WSLOPEN_INPUT""
-    wslopen_uri=$(wslopen_get_url_uri "$WSLOPEN_INPUT")
-elif [[ -f "$WSLOPEN_INPUT" ]]; then
-    [ $VERBOSE_MODE == 1 ] && echo "opened file "$WSLOPEN_INPUT""
-    wslopen_uri=$(wslopen_get_file_uri "$WSLOPEN_INPUT")
-elif [[ -d "$WSLOPEN_INPUT" ]]; then
-    [ $VERBOSE_MODE == 1 ] && echo "opened directory "$WSLOPEN_INPUT""
-    wslopen_uri=$(wslopen_get_dir_uri "$WSLOPEN_INPUT")
-else
-    echo "[ERROR] not a valid input: "$WSLOPEN_INPUT""
-    exit 1
-fi
-eval "$WIN_CMD /c start \"\" \"$wslopen_uri\" >/dev/null 2>/dev/null"
 
